330d215a527734570642e3d3b540b386d8a8e9bd
RPretet <125394075+RPretet@users.noreply.github.com>
2025-05-12 16:47:54 +0200

    tuto1

---
+++ TutoPart1/first.cpp
@@ -0,0 +1,21 @@
+#include <elm/io.h>
+#include <otawa/otawa.h>
+#include <otawa/app/Application.h>
+
+using namespace elm;
+using namespace otawa;
+
+class First: public Application {
+public:
+  First(void): Application("first", Version(1, 0, 0)) { }
+
+protected:
+  void work(const string &entry, PropList &props) override {
+    Address addr = workspace()->process()->findLabel(entry);
+    cout << entry << " found at " << addr << io::endl;
+  }
+
+    
+};
+
+OTAWA_RUN(First)
