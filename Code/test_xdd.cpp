079d707a80e99b9aa9d26598bae38d17fd34e0f7
Camille Vedani <camille.vedani@outlook.fr>
2025-05-13 11:29:41 +0200

    Mise à Jour des notes sur les XDD

--- Code/test_xdd.cpp
+++ Code/test_xdd.cpp
@@ -5,9 +5,9 @@ using namespace otawa::xengine;
 int main() {
     StandardXddManager man;
 
-    auto x = Xdd(&man.opsMan(), man.nMan().mK(3)); // variable 3
-    auto y = Xdd(&man.opsMan(), man.nMan().mK(1)); // variable 1
-    auto z = x + y; // union logique
+    auto x = Xdd(man.nMan(), 3);
+    auto y = Xdd(man.nMan(), 1);
+    auto z = x + y;
 
     return 0;
 }
