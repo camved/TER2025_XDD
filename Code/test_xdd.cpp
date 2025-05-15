7edd502a9f6ecbef8eaeeee147cfad9d6b1128aa
Camille Vedani <camille.vedani@outlook.fr>
2025-05-15 11:31:04 +0200

    Notes and codes to learn otawa

--- Code/test_xdd.cpp
+++ Code/test_xdd.cpp
@@ -1,13 +1,13 @@
-#include </home/camille/otawa/include/otawa/xdd/XddManager.hpp>
+// #include </home/camille/otawa/include/otawa/xdd/XddManager.hpp>
 
-using namespace otawa::xengine;
+// using namespace otawa::xengine;
 
-int main() {
-    StandardXddManager man;
+// int main() {
+//     StandardXddManager man;
 
-    auto x = Xdd(man.nMan(), 3);
-    auto y = Xdd(man.nMan(), 1);
-    auto z = x + y;
+//     auto x = Xdd(man.nMan(), 3);
+//     auto y = Xdd(man.nMan(), 1);
+//     auto z = x + y;
 
-    return 0;
-}
+//     return 0;
+// }
